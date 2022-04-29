a8a827df307d05447e3211425cb9ff897e5ab9de
Marc Rousavy <me@mrousavy.com>
2022-04-29 16:24:00 +0200

    exclusionList

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
-const escape = require('escape-string-regexp');
-const pak = require('../package.json');
+const path = require('path')
+const exclusionList = require('metro-config/src/defaults/exclusionList')
+const escape = require('escape-string-regexp')
+const pak = require('../package.json')
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '..')
 
 const modules = Object.keys({
   ...pak.peerDependencies,
-});
+})
 
 module.exports = {
   projectRoot: __dirname,
@@ -16,7 +16,7 @@ module.exports = {
   // We need to make sure that only one version is loaded for peerDependencies
   // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
+    blacklistRE: exclusionList(
       modules.map(
         (m) =>
           new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
@@ -24,8 +24,8 @@ module.exports = {
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
+      acc[name] = path.join(__dirname, 'node_modules', name)
+      return acc
     }, {}),
   },
 
@@ -37,4 +37,4 @@ module.exports = {
       },
     }),
   },
-};
+}
