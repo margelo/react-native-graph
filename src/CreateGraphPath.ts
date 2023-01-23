e327c9d0c823dc4c78e2f490f094e2f0f8c7d2f7
Thomas Coldwell <31568400+thomas-coldwell@users.noreply.github.com>
2023-01-23 14:55:34 +0000

    fix: Remove incorrect delta from pixel factor calculation (#56)
    * fix: Remove incorrect delta from pixel factor calculation
    
    * fix: Drawing factor calculation
    
    Co-authored-by: Christoph Pader <christoph@margelo.io>

--- src/CreateGraphPath.ts
+++ src/CreateGraphPath.ts
@@ -220,11 +220,9 @@ function createGraphPathBase({
 
     // Calculates how many points between two points must be
     // calculated and drawn onto the canvas
-    const drawingFactor = pixelFactorX(
-      new Date(point.date.getTime() - prev.date.getTime()),
-      range.x.min,
-      range.x.max
-    )
+    const spanX = range.x.max.getTime() - range.x.min.getTime()
+    const deltaX = point.date.getTime() - prev.date.getTime()
+    const drawingFactor = deltaX / spanX
     const drawingPixels = actualWidth * drawingFactor + horizontalPadding
     const numberOfDrawingPoints = Math.floor(drawingPixels / PIXEL_RATIO)
 
