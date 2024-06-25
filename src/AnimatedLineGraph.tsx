65f6afb7a0266cdb195bd55c3ca320830e112cc5
Benjamin Hakes <12475863+bhakes@users.noreply.github.com>
2024-06-25 09:46:39 -0500

    fix: set dashed line opacity to 0.4

--- src/AnimatedLineGraph.tsx
+++ src/AnimatedLineGraph.tsx
@@ -596,7 +596,7 @@ const getDashLineStyles = (color: string) => {
       marginVertical: 2,
       overflow: 'hidden',
       borderRadius: 1,
-      opacity: 0.5,
+      opacity: 0.4,
       backgroundColor: color,
     },
   })
